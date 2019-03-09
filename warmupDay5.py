903899caef2c9138c2409f368fe6bcda2be7ec6f
anagarcia02 <annertimes@gmail.com>
2019-03-09 15:33:10 +0000

    warmup anastasia day5

--- warmupDay5.py
+++ warmupDay5.py
@@ -1,1 +1,9 @@
 # File name: warmupDay5.py
+myName = "Anastasia"
+userGuess = input("Try to guess my name.")
+
+while userGuess != myName:
+    print("Sorry, that's not my name!")
+    userGuess = input("Try again.")
+else:
+    print("Congratulations! That's my name!")
